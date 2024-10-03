3c0fd4f1aeb70c0a706498c68af6dc4f25432689
HrsHristov <hrshristov@gmail.com>
2024-10-03 14:40:49 +0000

    add chars_dict

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ def main():
     book_path ="./books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
+    chars_dict = count_chars(text)
 
     print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 def count_words(text):
     words = text.split()
     words_count = len(words)
     return words_count
+
+def count_chars(text):
+    chars = {}
+    formatted_text = text.lower();
+
+    for char in formatted_text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+
+    return chars
 
 def get_book_text(path):
     with open(path) as f:
